62cd69efa490b68a12c8cffda65db853d3d66055
MaciekM <mjmm260@gmail.com>
2018-10-11 21:27:16 +0200

    initial commit

---
+++ src/main/java/Macias260/training/App.java
@@ -0,0 +1,13 @@
+package Macias260.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
